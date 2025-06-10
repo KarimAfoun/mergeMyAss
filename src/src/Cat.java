743ff8e65e041e83693a54874364236c85036b4e
Nima <m.rezaghani@gmail.com>
2025-06-10 15:38:13 +0200

    add catSay method

--- src/src/Cat.java
+++ src/src/Cat.java
@@ -7,4 +7,11 @@ public class Cat extends Animal {
     public void makeSound() {
         System.out.println(getName() + " says: Meow!");
     }
+
+    //_____________________nima______________________
+    public String catSays() {
+        return getName() + " says: Meow!";
+    }
+    //_____________________nima______________________
+
 }
