f11a713cbea2222f6075890bcdca6b0726a88b72
KarimAfoun <karimkafoun@gmail.com>
2025-06-10 16:11:34 +0200

    dasdasdasdsa

--- src/src/Main.java
+++ src/src/Main.java
@@ -2,9 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Animal animal = new Animal("Animal");
         animal.makeSound();
-        Cat cat = new Cat("Cat");
         Dog dog = new Dog("Dog");
         dog.makeSound();
-        cat.makeSound();
     }
 }
--- src/src/Rabit.java
+++ src/src/Rabit.java
@@ -2,9 +2,10 @@ public class Rabit extends Animal {
     public Rabit(String name) {
         super(name);
     }
+
     public void makeSound() {
         System.out.println("who is a rabbit");
-        System.out.println( getName() + " says:  CHICKEN NOT");
+        System.out.println(getName() + " says:  CHICKEN NOT");
     }
 
 }
