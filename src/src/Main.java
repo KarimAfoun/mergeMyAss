ecdea7908e96a9171a09ff61bbc74650b98ee0f3
KarimAfoun <karimkafoun@gmail.com>
2025-06-10 15:34:37 +0200

    start

---
+++ src/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal");
+        animal.makeSound();
+        Dog dog = new Dog("Dog");
+        dog.makeSound();
+        Cat cat = new Cat("Cat");
+        cat.makeSound();
+    }
+}
