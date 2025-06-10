b93437ea7cf0d245fb45d2e3675577f6737a7141
KarimAfoun <116573005+KarimAfoun@users.noreply.github.com>
2025-06-10 10:45:06 +0200

    start

---
+++ src/src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
